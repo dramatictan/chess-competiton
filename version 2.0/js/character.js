50da92eff7039424cae769f6132937ecb8855ff0
Tan Guan Ying <guanying.23@ichat.sp.edu.sg>
2025-04-03 19:59:42 +0800

    Expanded board

--- version 2.0/js/character.js
+++ version 2.0/js/character.js
@@ -4,11 +4,11 @@ const imagePlayers = document.querySelector("#imagePlayers")
 const selectedPlayer = document.querySelector("#selectedPlayer")
 
 const characters = [
-    {name: "BOT 1", image: "images/character/martin.png" , elo: 300},
-    {name: "BOT 2", image: "images/character/martin.png" , elo: 600},
+    {name: "Nikocado Avocado", image: "images/character/nikocado.jpeg" , elo: 300},
+    {name: "Scout", image: "images/character/scout.jpeg" , elo: 600},
     {name: "BOT 3", image: "images/character/martin.png" , elo: 800},
     {name: "BOT 4", image: "images/character/martin.png" , elo: 900},
-    {name: "BOT 5", image: "images/character/martin.png" , elo: 1000},
+    {name: "Spy", image: "images/character/spy.jpeg" , elo: 1000},
     {name: "BOT 6", image: "images/character/martin.png" , elo: 1200},
 ]
 
@@ -21,7 +21,7 @@ characters.forEach(character => {
     botImage.alt = character.name;
 
     const botName = document.createElement("p");
-    botName.textContent = `${character.name} (${character.elo})`;
+    botName.textContent = `${character.name} | ELO: (${character.elo})`;
 
     // Append the image and name to the bot container
     botContainer.appendChild(botImage);
